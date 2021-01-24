cec017141d1b182529ef0c8211b742403a3a625b
Michael <13236924+Mirobit@users.noreply.github.com>
2021-01-24 12:17:11 +0100

    New notification colors

--- frontend/assets/js/components/notification.js
+++ frontend/assets/js/components/notification.js
@@ -4,7 +4,7 @@ const displayNotification = (success, message) => {
   if (success === true) {
     Store.messageNode.innerHTML = `<div class="alert alert-success alert-dismissible" role="alert"><a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>${message}</div>`;
   } else {
-    Store.messageNode.innerHTML = `<div class="alert alert-warning alert-dismissible" role="alert"><a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>${message}</div>`;
+    Store.messageNode.innerHTML = `<div class="alert alert-error alert-dismissible" role="alert"><a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>${message}</div>`;
   }
 };
 
