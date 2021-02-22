540749a73c1e1596250895595f2e9023743a6f3b
Michael <13236924+Mirobit@users.noreply.github.com>
2021-02-22 23:03:03 +0100

    Fix eslint top-level await

--- server/app.js
+++ server/app.js
@@ -11,29 +11,34 @@ import routes from './routes/index.js';
 import wssInit from './services/socket.js';
 import dirname from './utils/dirname.cjs';
 
-mongoose.Promise = Promise;
-try {
-  await mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
+    console.log('Connected to mongo');
+  } catch (error) {
+    console.error('Error connecting to mongo', error);
+  }
+
+  const app = express();
+  const server = http.createServer(app);
+  wssInit(server);
+
+  app.use(express.json());
+  app.use(security);
+  app.use(express.static(path.join(dirname, '../../frontend/assets')));
+  app.use(routes);
+  app.use(errorHandler);
+
+  server.listen(process.env.PORT, () => {
+    console.log(
+      `Server is up and running: http://localhost:${process.env.PORT}`
+    );
   });
-  console.log('Connected to mongo');
-} catch (error) {
-  console.error('Error connecting to mongo', error);
-}
+};
 
-const app = express();
-const server = http.createServer(app);
-wssInit(server);
-
-app.use(express.json());
-app.use(security);
-app.use(express.static(path.join(dirname, '../../frontend/assets')));
-app.use(routes);
-app.use(errorHandler);
-
-server.listen(process.env.PORT, () => {
-  console.log(`Server is up and running: http://localhost:${process.env.PORT}`);
-});
+start();
